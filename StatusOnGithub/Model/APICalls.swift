7279697da4e2ad1768794ba94e0513b9ac9caa8b
Tprezioso <tommyprezioso@gmail.com>
2019-05-26 15:30:08 -0400

    adding the api call model to the app

---
+++ StatusOnGithub/Model/APICalls.swift
@@ -0,0 +1,15 @@
+//
+//  APICalls.swift
+//  StatusOnGithub
+//
+//  Created by Thomas Prezioso on 5/26/19.
+//  Copyright © 2019 Thomas Prezioso. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+import SwiftyJSON
+
+class APICall {
+    <#code#>
+}
